21de7ad40def5c43d93ae95e5a3b7b02f5acdd3a
rajeevco8119 <rajeev.co8119@gmail.com>
2023-09-29 17:11:04 +0530

    Create permutations.py

---
+++ test1/permutations.py
@@ -0,0 +1,40 @@
+# Recursion is used. split the list into sublist with 1 element and remaining elements and then add 1 element at start of each recursion.
+# Lets take ['a','b','c'] as example, take a first
+# a,bc
+# bc,a
+# Then swap b with a
+# b,ac
+# ac,b
+
+# Then swap c with a
+# c,ba
+# ba,c
+
+# Then swap b with c (Note use double loop to swap elements)
+# a,cb
+# cb,a
+
+def permutation(lst):
+    if len(lst) == 0:
+        return []
+
+    if len(lst) == 1:
+        return [lst]
+    l = []
+    for i in range(len(lst)):
+        m = lst[i]
+        remLst = lst[:i] + lst[i + 1:]
+
+        for p in permutation(remLst):
+            l.append([m] + p)
+
+    return l
+
+data = list('123')
+p = permutation(data)
+print(p)
+
+data = list('123')
+p = permutation(data)
+print(p)
+
